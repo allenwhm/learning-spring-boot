496375b6d1539e91c170fd9bcd6717c8369e4122
allenwhm <wanghmjlu@163.com>
2018-03-25 22:29:29 +0800

    try @GetMapping and @PostMapping

--- src/main/java/com/allenwhm/learningspringboot/HelloController.java
+++ src/main/java/com/allenwhm/learningspringboot/HelloController.java
@@ -1,18 +1,20 @@
 package com.allenwhm.learningspringboot;
 
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author allenwhm
  * @date 2018/3/25 20:48
  * @description
  */
-@Controller
-@ResponseBody
+//@Controller
+//@ResponseBody
+@RestController
+@RequestMapping("/go")
 public class HelloController {
 
     @Autowired
@@ -22,7 +24,9 @@ public class HelloController {
     private String boyName;
 
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @GetMapping(value = "/hello")
+//    @PostMapping(value = {"/hello", "/hi"})
+    // use Postman to test Post method
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return girl.getCupSize();
 //        return "index";
