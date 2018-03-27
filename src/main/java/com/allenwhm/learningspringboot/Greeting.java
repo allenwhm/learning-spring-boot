efeb9450748b27f4f88c8e0ab0c3bab582e1bd97
allenwhm <wanghmjlu@163.com>
2018-03-27 23:03:03 +0800

    rest service

---
+++ src/main/java/com/allenwhm/learningspringboot/Greeting.java
@@ -0,0 +1,25 @@
+package com.allenwhm.learningspringboot;
+
+/**
+ * @author allenwhm
+ * @date 2018/3/27 22:54
+ * @description
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/com/allenwhm/learningspringboot/GreetingController.java
@@ -0,0 +1,24 @@
+package com.allenwhm.learningspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author allenwhm
+ * @date 2018/3/27 22:56
+ * @description https://spring.io/guides/gs/rest-service/
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
