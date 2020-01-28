bac8d5b4294dbab5a4068aef0ed273e8adf32cee
allnwhm <wanghmjlu@163.com>
2020-01-28 21:45:50 +0800

    01-rest-service : replace with lombok

---
+++ 01-rest-service/src/main/java/com/allenwhm/rs/Greeting.java
@@ -0,0 +1,15 @@
+package com.allenwhm.rs;
+
+import lombok.Data;
+
+/**
+ * @author allenwhm
+ * @date 2018/3/27 22:54
+ * @description POJO
+ */
+@Data
+public class Greeting {
+
+    private final long id;
+    private final String content;
+}
--- 01-rest-service/src/main/java/com/allenwhm/rs/GreetingController.java
+++ 01-rest-service/src/main/java/com/allenwhm/rs/GreetingController.java
@@ -1,4 +1,4 @@
-package com.allenwhm.rest;
+package com.allenwhm.rs;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
