796218d11a2a974c26ab2cc5125f6b84b9d6105c
allnwhm <wanghmjlu@163.com>
2020-01-28 22:29:35 +0800

    01-rest-service: add unit test

---
+++ 01-rest-service/src/test/java/com/allenwhm/rs/GreetingControllerTest.java
@@ -0,0 +1,33 @@
+package com.allenwhm.rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GreetingControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void paramGreetingShouldReturnCustomizedMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Boot")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring Boot!"));
+    }
+}
