442bc9a1b9b389505c7fd94342856bff456757b1
allenwhm <550177619@qq.com>
2021-05-16 11:38:20 +0800

    update authenticating-a-user-with-ldap

--- authenticating-a-user-with-ldap/src/main/java/com/allenwhm/ldap/LdapApplication.java
+++ authenticating-a-user-with-ldap/src/main/java/com/allenwhm/ldap/LdapApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author: allenwhm
  * @date: 2020/3/23 0:44
+ * guide: https://spring.io/guides/gs/authenticating-ldap/
  */
 @SpringBootApplication
 public class LdapApplication {
